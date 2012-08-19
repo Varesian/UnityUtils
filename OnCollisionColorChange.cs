0d4a4950521beab4a0d973704c3dbb910278bf84
Cory Barr <cory.barr@gmail.com>
2012-08-19 03:14:27 -0700

    OnCollisionColorChange can now fade back to initial color instead of abrupt jump

--- OnCollisionColorChange.cs
+++ OnCollisionColorChange.cs
@@ -4,18 +4,39 @@ using System.Collections;
 public class OnCollisionColorChange : MonoBehaviour {
 	
 	public Color collidingColor;
+	public float colorFadeSecs = 0;
 	private Color initialColor;
+	private float timeLeftForColorFade;
+	private bool inCollision;
+	private bool isInCollision;
 	
 	void Start() {
 		initialColor = transform.renderer.material.color;
+		if (colorFadeSecs < 0) {
+			colorFadeSecs = 0;
+			Debug.LogWarning("colorFadeSecs cannot be negative. Setting to 0.", this);
+		}
+		timeLeftForColorFade = colorFadeSecs;
 	}
 	
 	void OnCollisionEnter() {
-		transform.renderer.material.color = collidingColor;
+		isInCollision = true;
+		renderer.material.color = collidingColor;
+		timeLeftForColorFade = colorFadeSecs;
 	}
 	
 	void OnCollisionExit() {
-		transform.renderer.material.color = initialColor;
+		isInCollision = false;
+		if (colorFadeSecs == 0) {
+			renderer.material.color = initialColor;
+		}
 	}
 	
+	void Update() {
+		if (!isInCollision && timeLeftForColorFade > 0) {
+			timeLeftForColorFade -= Time.deltaTime;
+			renderer.material.color = Color.Lerp(initialColor, collidingColor,
+				timeLeftForColorFade / colorFadeSecs);
+		}
+	}
 }
